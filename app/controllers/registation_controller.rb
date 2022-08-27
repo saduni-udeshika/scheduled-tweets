f446cf6fa39a74e73b1fb6c9efc2c06f5e912107
saduni-udeshika <saduniudeshika0@gmail.com>
2022-08-27 11:50:31 +0530

    Create sign up form

---
+++ app/controllers/registation_controller.rb
@@ -0,0 +1,5 @@
+class RegistationController < ApplicationController
+    def new
+        @user = User.new
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 #  GET /about
  get "about-us", to: "about#index", as: :about
+ get "sign-up", to: "registation#new"
+ post "sign-up", to: "registation#create"
 
  root to:"main#index"
 end
